7ae4b85b760b8ea944c361c9bd25ed0f9ad286c1
Josh McAdams <ikoniak@gmail.com>
2014-01-29 21:29:08 -0800

    Update keg.py

--- keg.py
+++ keg.py
@@ -56,10 +56,10 @@ def renderThings(flowMeter, flowMeter2, tweet, windowSurface, basicFont):
   windowSurface.blit(mug, (397,200))
   
   leftkeg = pygame.image.load('corny.png')
-  windowSurface.blit(leftkeg, (20,50))
+  windowSurface.blit(leftkeg, (20,30))
   
   rightkeg = pygame.image.load('corny.png')
-  windowSurface.blit(rightkeg, (900,50))
+  windowSurface.blit(rightkeg, (900,30))
   
   # Draw Beer Name Left Keg
   text = beerFont.render(LEFTBEER, True, WHITE, BLACK)
