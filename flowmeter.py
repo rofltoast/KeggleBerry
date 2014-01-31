84a64717111a447d03cefb3aa631e7d4f2645653
Josh McAdams <ikoniak@gmail.com>
2014-01-30 16:50:16 -0800

    Update flowmeter.py

--- flowmeter.py
+++ flowmeter.py
@@ -79,4 +79,4 @@ class FlowMeter():
       
   #Display cost for current pour
   def getFormattedPrice(self):
-    return str(round(self.getFormattedThisPour() * (self.grainBill / 40), 2))
+    return str(round((self.thisPour * FlowMeter.PINTS_IN_A_LITER) * (self.grainBill / 40), 2))
