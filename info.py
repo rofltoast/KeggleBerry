5fc6d26bc2dab65bb27e79e4be9d5f53ebc747c0
Josh McAdams <ikoniak@gmail.com>
2014-01-31 15:52:33 -0800

    Update info.py

--- info.py
+++ info.py
@@ -1,24 +1,22 @@
 class Info():
 #Left Beer vars
- beerNameL = "Indian Pale Ale"
- beerCalL = 10
- beerPriceL = 49
- beerInfoL = ""
- beerTotalL = 40
- beerPicL = ""
- beerAboutL = ""
- beerIngL = ""
- beerABVL = 0
- SRML = ''
+ beerNameL = "Indian Pale Ale" #Left beer name
+ beerCalL = 10 #Left beer calories
+ beerPriceL = 49 #Left beer grainbill
+ beerTotalL = 40 #Left beer total beer in PINTS
+ beerPicL = "" #Left beer label
+ beerAboutL = "" #Left beer synopsis
+ beerIngL = "" #Left beer ingredients
+ beerABVL = 0 #Left beer ABV
+ SRML = '' #Left beer SRM
 
  #Right Beer vars
- beerNameR = "Black IPA"
- beerCalR = 89
- beerPriceR = 31
- beerInfoR = ""
- beerTotalR = 34
- beerPicR = ""
- beerAboutR = ""
- beerIngR = ""
- beerABVR = 0
- SRMR = ''
+ beerNameR = "Black IPA" #Right beer name
+ beerCalR = 89 #Right beer calories
+ beerPriceR = 31 #Right beer grainbill
+ beerTotalR = 34 #Right beer total beer in PINTS
+ beerPicR = "" #Right beer label
+ beerAboutR = "" #Right beer synopsis
+ beerIngR = "" #Right beer ingredients
+ beerABVR = 0 #Right beer ABV
+ SRMR = ''  #Right beer SRM
