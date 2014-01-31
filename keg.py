84c0ad7140bb5a7ab0f4497223f0f247bef83311
Josh McAdams <ikoniak@gmail.com>
2014-01-31 14:07:31 -0800

    Update keg.py

--- keg.py
+++ keg.py
@@ -133,13 +133,13 @@ while True:
   currentTime = int(time.time() * FlowMeter.MS_IN_A_SECOND)
   
   if (fm.thisPour > 0.05 and currentTime - fm.lastClick > 2000): # 2 seconds of inactivity causes a tweet
-    tweet = "Someone just poured " + fm.getFormattedThisPour() + " of " + LEFTBEER + " from the keg! ($" + fm.getFormattedPrice()
+    tweet = "Someone just poured " + fm.getFormattedThisPour() + " of " + LEFTBEER + " from the keg! ($" + fm.getFormattedPrice() + ") only " + fm.getFormattedRemaining() + " remaining!"
     ######insert SQL push here(thisPour)
     fm.thisPour = 0.0
     tweetPour(tweet)
  
   if (fm2.thisPour > 0.05 and currentTime - fm2.lastClick > 2000): # 2 seconds of inactivity causes a tweet
-    tweet = "Someone just poured " + fm2.getFormattedThisPour() + " of " + RIGHTBEER + " from the keg! ($" + fm2.getFormattedPrice() + ")"
+    tweet = "Someone just poured " + fm2.getFormattedThisPour() + " of " + RIGHTBEER + " from the keg! ($" + fm2.getFormattedPrice() + ") only " + fm2.getFormattedRemaining() + " remaining!"
     ######insert SQL push here(thisPour)
     fm2.thisPour = 0.0
     tweetPour(tweet)
