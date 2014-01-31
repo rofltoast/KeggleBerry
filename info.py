dc197653b6e19c1ae8051b0274b778f520bd8cf6
Josh McAdams <ikoniak@gmail.com>
2014-01-31 12:34:00 -0800

    Update info.py

--- info.py
+++ info.py
@@ -1,24 +1,24 @@
-Class Info():
+class Info():
 #Left Beer vars
-beerNameL = ""
-beerCalL = 10
-beerPriceL = 49
-beerInfoL = ""
-beerTotalL = 40
-beerPicL = ""
-beerAboutL = ""
-beerIngL = ""
-beerABVL = 0
-SRML = ''
+ beerNameL = ""
+ beerCalL = 10
+ beerPriceL = 49
+ beerInfoL = ""
+ beerTotalL = 40
+ beerPicL = ""
+ beerAboutL = ""
+ beerIngL = ""
+ beerABVL = 0
+ SRML = ''
 
-#Right Beer vars
-beerNameR = ""
-beerCalR = 89
-beerPriceR = 31
-beerInfoR = ""
-beerTotalR = 34
-beerPicR = ""
-beerAboutR = ""
-beerIngR = ""
-beerABVR = 0
-SRMR = ''
+ #Right Beer vars
+ beerNameR = ""
+ beerCalR = 89
+ beerPriceR = 31
+ beerInfoR = ""
+ beerTotalR = 34
+ beerPicR = ""
+ beerAboutR = ""
+ beerIngR = ""
+ beerABVR = 0
+ SRMR = ''
